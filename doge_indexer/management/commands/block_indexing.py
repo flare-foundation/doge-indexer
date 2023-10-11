af5ed9a1cfb94fdf716afb4a271376d1a95ae0e7
Luka Avbreht <luka.avbreht@aflabs.si>
2023-10-11 09:46:44 +0200

    updated indexer running

--- doge_indexer/management/commands/block_indexing.py
+++ doge_indexer/management/commands/block_indexing.py
@@ -2,10 +2,6 @@ from django.core.management.base import BaseCommand
 
 from doge_indexer.indexer import DogeIndexerClient, DogeIndexerClientConfig
 
-# txid = "19aeaa88859c04a333257f1119a77438ac08feec424c6ad3645a0679c8be9882"
-# txid = "c8aafe466c59292f74dc9e3c8cc82fdda16edcf5d656d13b73219f96ff7b1d82"
-txid = "95bf6eb2d5be272fc42b03cebc258a36e5f9c62079823208bd3026d6ed75e070"
-
 client_config: DogeIndexerClientConfig = {
     "url": "http://213.32.6.191:22555/",
     "username": "admin",
@@ -20,8 +16,4 @@ client_config: DogeIndexerClientConfig = {
 class Command(BaseCommand):
     def handle(self, *args, **options):
         indexer = DogeIndexerClient(client_config)
-        import time
-
-        start = time.time()
         indexer.run()
-        print("Full index in ", time.time() - start)
