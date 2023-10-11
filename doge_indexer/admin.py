7a8e41539eddbdc80b48d65ffddcb6081713b965
Luka Avbreht <luka.avbreht@aflabs.si>
2023-10-11 09:44:11 +0200

    updated doge indexer

--- doge_indexer/admin.py
+++ doge_indexer/admin.py
@@ -5,6 +5,7 @@ from doge_indexer.models import (
     TransactionInput,
     TransactionInputCoinbase,
     TransactionOutput,
+    DogeBlock
 )
 
 
@@ -38,3 +39,10 @@ class DogeTransactionAdmin(admin.ModelAdmin):
     ordering = ("-timestamp",)
 
     inlines = (TransactionInputCoinbaseInline, TransactionInputInline, TransactionOutputInline)
+
+
+@admin.register(DogeBlock)
+class DogeBlockAdmin(admin.ModelAdmin):
+    list_display = ("block_number", "timestamp", "block_hash", "transactions")
+    search_fields = ("block_number","block_hash")
+    ordering = ("-timestamp",)
---
+++ doge_indexer/views/other_views.py
@@ -0,0 +1,55 @@
+# TODO:
+
+# 1. State api
+#   "data": [
+#     {
+#       "valueString": "",
+#       "valueNumber": 2531739,
+#       "timestamp": 1696922135,
+#       "comment": "",
+#       "name": "btc_N"
+#     },
+#     {
+#       "valueString": "",
+#       "valueNumber": 2530675,
+#       "timestamp": 1696819114,
+#       "comment": "",
+#       "name": "btc_Nbottom"
+#     },
+#     {
+#       "valueString": "",
+#       "valueNumber": 1696606744,
+#       "timestamp": 1696819114,
+#       "comment": "",
+#       "name": "btc_NbottomTime"
+#     },
+#     {
+#       "valueString": "running",
+#       "valueNumber": 95539,
+#       "timestamp": 1696922268,
+#       "comment": "N=2531739 T=2531745",
+#       "name": "btc_state"
+#     },
+#     {
+#       "valueString": "",
+#       "valueNumber": 2531745,
+#       "timestamp": 1696922135,
+#       "comment": "",
+#       "name": "btc_T"
+#     }
+#   ]
+
+# 2. Block range
+# {
+#   "status": "OK",
+#   "data": {
+#     "first": 2530675,
+#     "last": 2531739
+#   }
+# }
+
+# 3. Confirmed block at (to so pr nas vsi)
+
+# 4. block height (to je block range last)
+
+# 5. Block that has transaction hash
