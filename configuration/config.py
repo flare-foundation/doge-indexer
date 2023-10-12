0bebc7c3bdf0033acfb910fe03eb0ece291cac18
Matej Janežič <janezic.mj@gmail.com>
2023-10-12 18:33:06 +0200

    feat: add block pruning

--- configuration/config.py
+++ configuration/config.py
@@ -14,6 +14,8 @@ def get_config() -> Config:
     number_of_block_confirmations = int(os.environ.get("NUMBER_OF_BLOCK_CONFIRMATIONS", "60"))
     initial_block_height = int(os.environ.get("INITIAL_BLOCK_HEIGHT", "1"))
     number_of_workers = int(os.environ.get("NUMBER_OF_WORKERS", "10"))
+    prune_keep_days = int(os.environ.get("PRUNE_KEEP_DAYS", "0"))
+    prune_interval_seconds = int(os.environ.get("PRUNE_INTERVAL_SECONDS", "60"))
 
     return Config(
         NODE_RPC_URL=node_rpc_url,
@@ -23,6 +25,8 @@ def get_config() -> Config:
         NUMBER_OF_BLOCK_CONFIRMATIONS=number_of_block_confirmations,
         INITIAL_BLOCK_HEIGHT=initial_block_height,
         NUMBER_OF_WORKERS=number_of_workers,
+        PRUNE_KEEP_DAYS=prune_keep_days,
+        PRUNE_INTERVAL_SECONDS=prune_interval_seconds,
     )
 
 
